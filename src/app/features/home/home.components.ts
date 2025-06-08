885f08484e8aee0a3403fa5391fc871f30c63fc9
114080-Rossi-Braian <114080@tecnicatura.frc.utn.edu.ar>
2025-06-08 19:16:19 -0300

    Load Game

--- src/app/features/home/home.components.ts
+++ src/app/features/home/home.components.ts
@@ -1,11 +1,39 @@
 import {GameService, NewGameRequestDTO} from 'app/core/services/game.service';
-
+enum Colors {
+  RED = 'RED',
+  BLUE = 'BLUE',
+  GREEN = 'GREEN',
+  YELLOW = 'YELLOW',
+  PURPLE = 'PURPLE'
+}
+enum DifficultyLevel {
+  PROFESIONAL = 'PROFESIONAL',
+  MEDIUM = 'MEDIUM',
+  AMATEUR = 'AMATEUR'
+}
+enum GameState {
+  ESPERANDO_JUGADORES = 'ESPERANDO_JUGADORES',
+  REPARTO_TERRITORIOS = 'REPARTO_TERRITORIOS',
+  ASIGNACION_OBJETIVOS = 'ASIGNACION_OBJETIVOS',
+  RONDA_1 = 'RONDA_1',
+  RONDA_2 = 'RONDA_2',
+  RONDA_3 = 'RONDA_3',
+  TURNO_JUGADOR = 'TURNO_JUGADOR',
+  FIN_JUEGO = 'FIN_JUEGO'
+}
+enum TurnState {
+  STARTED = 'STARTED',
+  FINISHED = 'FINISHED',
+  PAUSE = 'PAUSE',
+  CANCELED = 'CANCELED'
+}
 interface GameConfig {
-  id: string
-  color: string
-  difficulty: string
-  date: Date
-  status: string
+  gameId: number;
+  gameDifficulty: DifficultyLevel;
+  gameState: GameState;
+  currentTurnPlayerId: number | null;
+  createdByPlayerId: number;
+  turnState: TurnState;
 }
 
 import { Component } from '@angular/core';
@@ -29,11 +57,11 @@ export class HomeComponent {
   currentDate: Date = new Date();
 
   colorOptions = [
-    { value: "red", label: "Rojo" },
-    { value: "blue", label: "Azul" },
-    { value: "green", label: "Verde" },
-    { value: "yellow", label: "Amarillo" },
-    { value: "purple", label: "Morado" }
+    { value: "RED", label: "RED" },
+    { value: "BLUE", label: "BLUE" },
+    { value: "GREEN", label: "GREEN" },
+    { value: "YELLOW", label: "YELLOW" },
+    { value: "PURPLE", label: "PURPLE" }
   ]
 
   difficultyOptions = [
@@ -60,6 +88,7 @@ export class HomeComponent {
   onLoadGame() {
     this.showLoadGameList = true
     this.showNewGameForm = false
+    this.loadSavedGames()
   }
 
   onStartNewGame() {
@@ -68,12 +97,13 @@ export class HomeComponent {
       medium: 2,
       expert: 3
     };
+
     const colorMap: { [key: string]: number } = {
-      red: 1,
-      blue: 2,
-      green: 3,
-      yellow: 4,
-      purple: 5
+      RED: 1,
+      BLUE: 2,
+      GREEN: 3,
+      YELLOW: 4,
+      PURPLE: 5
     }
     const selectedDifficulty = this.newGameForm.value.difficulty;
     const selectedColor = this.newGameForm.value.color;
@@ -97,8 +127,8 @@ export class HomeComponent {
     this.showLoadGameList = false
   }
 
-  onDeleteSavedGame(gameId: string) {
-    this.savedGames = this.savedGames.filter(game => game.id !== gameId)
+  onDeleteSavedGame(gameId: number) {
+    this.savedGames = this.savedGames.filter(game => game.gameId !== gameId)
     this.updateLocalStorage()
   }
 
@@ -117,15 +147,27 @@ export class HomeComponent {
     this.updateLocalStorage()
   }
 
-  private loadSavedGames() {
-    console.log('Loading existing game...');
-    const saved = localStorage.getItem("savedGames")
-    if (saved) {
-      this.savedGames = JSON.parse(saved).map((game: any) => ({
-        ...game,
-        date: new Date(game.date)
-      }))
-    }
+  //savedGames: NewGameResponseDTO[] = [];
+
+  loadSavedGames() {
+    const playerId = 49; // Reemplaza con el ID del jugador actual
+    //TODO todavia me falta esta parte, por ahora lo dejo hardcodeado
+    this.gameService.getGamesByPlayer(playerId).subscribe({
+      next: (games) => {
+        this.savedGames = games.map(game => ({
+          gameId: game.gameId,
+          gameDifficulty: game.gameDifficulty as DifficultyLevel,
+          gameState: game.gameState as GameState,
+          currentTurnPlayerId: game.currentTurnPlayerId,
+          createdByPlayerId: game.createdByPlayerId,
+          turnState: game.turnState as TurnState
+        }));
+      },
+      error: (err) => {
+        console.error('Error al cargar juegos', err);
+        this.savedGames = [];
+      }
+    });
   }
 
   private updateLocalStorage() {
