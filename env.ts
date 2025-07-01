9356b1aa4e4dd89af2ce6fc3f7d8d5e4f91d16be
114080-Rossi-Braian <114080@tecnicatura.frc.utn.edu.ar>
2025-07-01 19:09:21 -0300

    update Forwarding

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 // URLs para uso directo (si no usás Netlify o el proxy)
 
 const developmentUrl = 'http://localhost:8081';
-const productionUrl = 'https://4068-2803-9800-9897-91b5-44f9-3da9-7530-df6d.ngrok-free.app';  // actualizable si cambia`
+const productionUrl = 'https://7fc1-190-114-208-144.ngrok-free.app';  // actualizable si cambia`
 
 // Si production es false, backUrl será developmentUrl`
 // Si production es true, backUrl será productionUrl`
